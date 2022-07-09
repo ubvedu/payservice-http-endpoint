8c8124302afde68fa6ed4138901ee7567e53ebae
Roman Ukhobotov <ukhobotov.rv@phystech.edu>
2022-07-09 08:06:52 +0300

    integrate Config parameters into Server

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,10 @@ import (
 )
 
 func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("usage: go run ./cmd <config path>")
+        os.Exit(1)
+    }
     confPath := os.Args[1]
     conf, err := coreConfig.Parse(confPath)
     if err != nil {
@@ -19,7 +23,12 @@ func main() {
     }
     config.BuildDI(conf)
 
-    port := "8080"
-    log.Printf("Serving at: http://localhost:%s\n", port)
-    log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%s", port), endpoint.NewRouter()))
+    server := &http.Server{
+        Addr:         ":" + conf.Http.Port,
+        Handler:      endpoint.NewRouter(),
+        ReadTimeout:  conf.Http.ReadTimeout,
+        WriteTimeout: conf.Http.WriteTimeout,
+    }
+    log.Printf("Serving at: http://localhost:%s\n", conf.Http.Port)
+    log.Fatalln(server.ListenAndServe())
 }
