77284fe2708112ed40b3c7d43120d0dc6baa901c
Roman Ukhobotov <ukhobotov.rv@phystech.edu>
2022-07-09 07:38:37 +0300

    moved router outside main function

--- main.go
+++ main.go
@@ -4,13 +4,10 @@ import (
     coreConfig "core-payment-lesson/config"
     _ "embed"
     "fmt"
-    "github.com/gorilla/mux"
     "log"
     "net/http"
     "os"
     "payservice-http-endpoint/config"
-    "payservice-http-endpoint/handlers"
-    "payservice-http-endpoint/middleware"
 )
 
 func main() {
@@ -21,30 +18,7 @@ func main() {
     }
     config.BuildDI(conf)
 
-    router := mux.NewRouter()
-
-    auth := router.Queries("access_token", "{token}").Subrouter()
-    auth.Use(middleware.Auth)
-
-    // example:
-    //
-    //		/charge?access_token=0f62fe
-    //
-    // must contain json body:
-    //
-    // 		{
-    //		    "amount": 100,
-    //		    "terminalId": "x",
-    //		    "invoiceId": "y",
-    //		    "description": "aboba"
-    //		}
-    //
-    auth.
-        HandleFunc("/charge", handlers.Charge).
-        Methods(http.MethodPost).
-        Headers("Content-Type", "application/json")
-
     port := "8080"
     log.Printf("Serving at: http://localhost:%s\n", port)
-    log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
+    log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%s", port), NewRouter()))
 }
---
+++ router.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "github.com/gorilla/mux"
+    "net/http"
+    "payservice-http-endpoint/handlers"
+    "payservice-http-endpoint/middleware"
+)
+
+func NewRouter() http.Handler {
+
+    router := mux.NewRouter()
+
+    auth := router.Queries("access_token", "{token}").Subrouter()
+    auth.Use(middleware.Auth)
+
+    // example:
+    //
+    //		/charge?access_token=0f62fe
+    //
+    // must contain json body:
+    //
+    // 		{
+    //		    "amount": 100,
+    //		    "terminalId": "x",
+    //		    "invoiceId": "y",
+    //		    "description": "aboba"
+    //		}
+    //
+    auth.
+        HandleFunc("/charge", handlers.Charge).
+        Methods(http.MethodPost).
+        Headers("Content-Type", "application/json")
+
+    return router
+}
